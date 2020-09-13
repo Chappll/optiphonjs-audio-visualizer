0e07f118f085e08c2c59998ee90cd4dce4fe4606
Chappll <68033975+Chappll@users.noreply.github.com>
2020-09-13 14:11:59 +0100

    visualizer created

---
+++ src/Visualizer/index.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Canvas from './Canvas';
+
+const Visualizer = () => {
+    return <>
+    <header className="App-header">
+        <Canvas />
+    </header>
+    </>
+}
+
+export default Visualizer;
