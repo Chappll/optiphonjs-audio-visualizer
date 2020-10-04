8750acb539329b366c114b681c4d0df250bdf7ff
Chappll <68033975+Chappll@users.noreply.github.com>
2020-10-05 00:20:53 +0100

    05/10/2020
    useEffect added, analyser created

--- src/Visualizer/Canvas.jsx
+++ src/Visualizer/Canvas.jsx
@@ -1,5 +1,7 @@
 import songFile from '../audio/MarchingBands.mp3'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Slider from '@material-ui/core/Slider';
+
 
 function Canvas() {
     //const audioElement = new Audio(songFile);
@@ -7,6 +9,15 @@ function Canvas() {
     const [volume, setVolume] = React.useState(0.2)
     audio.volume = volume;
     const [playSong, setPlay] = React.useState(false)
+
+    useEffect(() => {
+        const context = new AudioContext()
+        const src = context.createMediaElementSource(audio)
+        const analyser = context.createAnalyser()
+        src.connect(analyser)
+        analyser.connect(context.destination)
+        const frequency_array = new Uint8Array(analyser.frequencyBinCount);
+      }, []);
 
     function songSelect() {
         
@@ -25,12 +36,22 @@ function Canvas() {
         console.log('playing song')   
         console.log(playSong)   
     }
+
+    const handleChange = (event, newValue) => {
+        setVolume(newValue);
+      };
+    
+      
+
     return(
         // <button onClick={song.play}>Play</button>
         <div>
             <button onClick={songSelect()}>Select song</button>
             <button onClick={playSong ? songPause : songPlay}>{playSong ? 'Pause' : 'Play'}</button>
             
+            <div>
+                <Slider min={0} max={1} step={0.01} onChange={handleChange} ></Slider>
+            </div>
         </div>
     );   
 }
